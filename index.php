8c85b18b4ca45409ee42b2b3a68d1fabac024882
ashir245 <117296966+ashir245@users.noreply.github.com>
2023-10-02 13:38:16 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Set the content type header to indicate plain text
+header("Content-Type: text/plain");
+
+// Your normal text to be rendered
+$text = "This is a sample text that will be rendered as plain text.";
+
+// Output the text
+echo $text;
+?>
